cecdccda85c6838eb937562dcc29e4fb52f87a08
hanchchch <cndghks15@gmail.com>
2022-12-27 22:36:38 +0900

    fix: title and description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Hanch</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Hanch Han (Chung-Hwan Han)" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
